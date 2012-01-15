77326e8ef3115da90476ce6413663b2b611b52b2
Eido NABESHIMA <closer009@gmail.com>
2012-01-15 20:53:30 +0900

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,3 +1,19 @@
 listen '/tmp/unicorn.sock'
 rails_env = ENV['RAILS_ENV'] || 'production'
 preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
